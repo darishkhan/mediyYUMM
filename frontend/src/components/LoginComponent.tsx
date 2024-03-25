24745c51eadb88bf7efc360806b755efdfaf7ed3
darishkhan <darishkhanon155@gmail.com>
2024-03-25 19:27:36 +0530

    backend cors added and login and signup

--- frontend/src/components/LoginComponent.tsx
+++ frontend/src/components/LoginComponent.tsx
@@ -3,65 +3,78 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-const URL = "https://backend.mediyyumm.workers.dev"
+const URL = "https://backend.mediyyumm.workers.dev";
 
+export const LoginComponent = () => {
+  const [signinInput, setSigninInput] = useState<SigninInput>({
+    email: "",
+    password: "",
+  });
 
-export const LoginComponent = ()=>{
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSigninInput({ ...signinInput, [e.target?.name]: e.target?.value });
+  };
 
-    const [loginData, setLoginData] = useState<SigninInput>({
-        email:"",
-        password:""
-    });
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.get(
+        `${URL}/api/v1/user/signin`,
+        signinInput
+      );
+    } catch (error) {}
+  };
 
-    const handleChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
-        setLoginData({...loginData, [e.target?.name]:e.target?.value});
-    }
-
-    useEffect(()=>{
-        console.log(loginData);
-    }, [loginData])
-        
-
-    const loginRequest = async ()=>{
-        try {
-            const response = await axios.get(`${URL}/login`, loginData);
-
-        } catch (error) {
-            
-        }
-    }
-
-
-    return(
-        <div className="flex justify-center min-h-screen items-center">
-            <div>
-            <div>
-            <div className="text-3xl font-bold text-center">Login</div>
-            <div className="text-center">Don't have an account? <Link to="/signup"><u>Sign Up</u></Link></div>
-            </div>
-            <div className="mt-5">
-                <div className="my-1">
-                    <div className="my-1 font-semibold"><label>
-                        Email
-                    </label></div>
-                    <div><input className="border border-gray-400 rounded-md px-2 w-full py-1 placeholder:text-sm" placeholder="johndoe@gmail.com" onChange={handleChange}>
-                    </input>
-                    </div>
-                </div>
-                <div className="my-1">
-                    <div className="my-1 font-semibold"><label>
-                        Password
-                    </label></div>
-                    <div><input className="border border-gray-400 rounded-md px-2 w-full py-1 placeholder:text-sm" placeholder="********" onChange={handleChange}>
-                    </input>
-                    </div>
-                </div>
-                <div className="my-5">
-                    <button className="w-full px-auto py-2 bg-black text-white font-semibold rounded-md">Log In</button>
-                </div>
-            </div>
-            
-            </div>
+  return (
+    <div className="flex justify-center min-h-screen items-center">
+      <div>
+        <div>
+          <div className="text-3xl font-bold text-center">Login</div>
+          <div className="text-center">
+            Don't have an account?{" "}
+            <Link to="/signup">
+              <u>Sign Up</u>
+            </Link>
+          </div>
         </div>
-    )
-}
+        <div className="mt-5">
+          <div className="my-1">
+            <div className="my-1 font-semibold">
+              <label>Email</label>
+            </div>
+            <div>
+              <input
+                className="border border-gray-400 rounded-md px-2 w-full py-1 placeholder:text-sm"
+                placeholder="johndoe@gmail.com"
+                onChange={handleChange}
+                name="email"
+                value={signinInput.email}
+              ></input>
+            </div>
+          </div>
+          <div className="my-1">
+            <div className="my-1 font-semibold">
+              <label>Password</label>
+            </div>
+            <div>
+              <input
+                className="border border-gray-400 rounded-md px-2 w-full py-1 placeholder:text-sm"
+                placeholder="********"
+                onChange={handleChange}
+                name="password"
+                value={signinInput.password}
+              ></input>
+            </div>
+          </div>
+          <div className="my-5">
+            <button
+              className="w-full px-auto py-2 bg-black text-white font-semibold rounded-md"
+              onClick={handleSubmit}
+            >
+              Log In
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- frontend/src/components/SignupComponent.tsx
+++ frontend/src/components/SignupComponent.tsx
@@ -1,45 +1,107 @@
-import { Link } from "react-router-dom"
+import { SignupInput } from "@darishkhan/mediyyumm-common/dist";
+import axios from "axios";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
+const URL = "https://backend.mediyyumm.workers.dev";
 
-export const SignupComponent = ()=>{
-    return(
-        <div className="flex justify-center min-h-screen items-center">
-            <div>
-            <div>
-            <div className="text-3xl font-bold text-center">Create an account</div>
-            <div className="text-center">Already have an account? <Link to="/login"><u>Sign up</u></Link></div>
-            </div>
-            <div className="mt-5">
-                <div className="my-1">
-                    <div className="my-1 font-semibold"><label>
-                        Name
-                    </label></div>
-                    <div><input className="border border-gray-400 rounded-md px-2 w-full py-1 placeholder:text-sm" placeholder="John Doe">
-                    </input>
-                    </div>
-                </div>
-                <div className="my-1">
-                    <div className="my-1 font-semibold"><label>
-                        Email
-                    </label></div>
-                    <div><input className="border border-gray-400 rounded-md px-2 w-full py-1 placeholder:text-sm" placeholder="johndoe@gmail.com">
-                    </input>
-                    </div>
-                </div>
-                <div className="my-1">
-                    <div className="my-1 font-semibold"><label>
-                        Password
-                    </label></div>
-                    <div><input className="border border-gray-400 rounded-md px-2 w-full py-1 placeholder:text-sm" placeholder="********">
-                    </input>
-                    </div>
-                </div>
-                <div className="my-5">
-                    <button className="w-full px-auto py-2 bg-black text-white font-semibold rounded-md">Sign up</button>
-                </div>
-            </div>
-            
-            </div>
+export const SignupComponent = () => {
+  const navigate = useNavigate();
+
+  const [signupInput, setSignupInput] = useState<SignupInput>({
+    email: "",
+    password: "",
+    name: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSignupInput({ ...signupInput, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post(
+        `${URL}/api/v1/user/signup`,
+        signupInput
+      );
+      if(response.status===200)
+      {
+          console.log(response);
+          localStorage.setItem('token', "Bearer "+response.data.token);
+          navigate("/blogs");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="flex justify-center min-h-screen items-center">
+      <div>
+        <div>
+          <div className="text-3xl font-bold text-center">
+            Create an account
+          </div>
+          <div className="text-center">
+            Already have an account?{" "}
+            <Link to="/login">
+              <u>Sign up</u>
+            </Link>
+          </div>
         </div>
-    )
-}
+        <div className="mt-5">
+          <div className="my-1">
+            <div className="my-1 font-semibold">
+              <label>Name</label>
+            </div>
+            <div>
+              <input
+                className="border border-gray-400 rounded-md px-2 w-full py-1 placeholder:text-sm"
+                placeholder="John Doe"
+                name="name"
+                value={signupInput.name}
+                onChange={handleChange}
+              ></input>
+            </div>
+          </div>
+          <div className="my-1">
+            <div className="my-1 font-semibold">
+              <label>Email</label>
+            </div>
+            <div>
+              <input
+                className="border border-gray-400 rounded-md px-2 w-full py-1 placeholder:text-sm"
+                placeholder="johndoe@gmail.com"
+                name="email"
+                value={signupInput.email}
+                onChange={handleChange}
+              ></input>
+            </div>
+          </div>
+          <div className="my-1">
+            <div className="my-1 font-semibold">
+              <label>Password</label>
+            </div>
+            <div>
+              <input
+                className="border border-gray-400 rounded-md px-2 w-full py-1 placeholder:text-sm"
+                placeholder="********"
+                name="password"
+                value={signupInput.password}
+                onChange={handleChange}
+              ></input>
+            </div>
+          </div>
+          <div className="my-5">
+            <button
+              className="w-full px-auto py-2 bg-black text-white font-semibold rounded-md"
+              onClick={handleSubmit}
+            >
+              Sign up
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
